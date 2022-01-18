38a77dee2c8778e43da5a3b69e79efed363bf2b8
Nirav-Khanpara <97885050+Nirav-Khanpara@users.noreply.github.com>
2022-01-18 10:47:38 +0530

    Update python_basics_assignment.py

--- Python-Basics/Assignment/python_basics_assignment.py
+++ Python-Basics/Assignment/python_basics_assignment.py
@@ -2,41 +2,40 @@ import random
 import string
 import asyncio
 
-async def generate_random(s_minimum,s_maximum):
-    x=random.choice([i for i in range(s_minimum,s_maximum+1)])
+async def generate_random(length=6):
     s=''
-    for _ in range(x):
+    for _ in range(length):
         s+=random.choice(string.ascii_letters)
     return s
 
-async def write_count(file_name,c):
-    # await asyncio.sleep(0.1)
+async def write_count(c):
     with open('counts.log', 'w') as f:
         f.write(f"No. of time keyword 'MARUTI' occured in file1: {c[0]}" + '\n')
         f.write(f"No. of time keyword 'MARUTI' occured in file2: {c[1]}")
 
-async def write_to_file(file_name,s_minimum,s_maximum,c):
-    generated_string = random.choice([await generate_random(s_minimum,s_maximum), 'MARUTI'])
+async def read_files(fname,c):
+    with open(fname,'r') as f:
+        for i in f.readlines():
+            if i=='MARUTI\n':
+                if fname=='File1.txt':
+                    c[0]+=1
+                else:
+                    c[1]+=1
+
+async def write_to_file(file_name,c):
+    generated_string = random.choice([await generate_random(), 'MARUTI'])
     with open(file_name,'a') as f:
         f.write(generated_string+'\n')
-    if generated_string=='MARUTI':
-        if file_name=='File1.txt':
-            c[0]+=1
-        else:
-            c[1]+=1
-    await write_count(file_name,c)
 
 async def main():
-    c = [0, 0]
-    s_minimum = int(input('Enter minimum length for random string: '))
-    s_maximum = int(input('Enter maximum length for random string: '))
-    if s_maximum < s_minimum:
-        print('maximum length should be greater than or equal to minimum length')
-        print('Try runnig program again !')
-    else:
-        while True:
-            t1 = asyncio.create_task(write_to_file('File1.txt',s_minimum,s_maximum,c))
-            t2 = asyncio.create_task(write_to_file('File2.txt',s_minimum,s_maximum,c))
-            await asyncio.gather(t1, t2)
+    while True:
+        c = [0, 0]
+        t1 = asyncio.create_task(write_to_file('File1.txt',c))
+        t2 = asyncio.create_task(write_to_file('File2.txt',c))
+        t3 = asyncio.create_task(read_files('File1.txt',c))
+        t4 = asyncio.create_task(read_files('File2.txt',c))
+        t5 = asyncio.create_task(write_count(c))
+
+        await asyncio.gather(t1, t2, t3, t4, t5)
 
 asyncio.run(main())
