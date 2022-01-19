9e8ab905c128f6b8cfc9ad48cdcffc07e68265eb
Nirav-Khanpara <97885050+Nirav-Khanpara@users.noreply.github.com>
2022-01-19 17:27:26 +0530

    Add files via upload

--- Python-Basics/Assignment/python_basics_assignment.py
+++ Python-Basics/Assignment/python_basics_assignment.py
@@ -13,29 +13,29 @@ async def write_count(c):
         f.write(f"No. of time keyword 'MARUTI' occured in file1: {c[0]}" + '\n')
         f.write(f"No. of time keyword 'MARUTI' occured in file2: {c[1]}")
 
-async def read_files(fname,c):
-    with open(fname,'r') as f:
-        for i in f.readlines():
-            if i=='MARUTI\n':
-                if fname=='File1.txt':
-                    c[0]+=1
-                else:
-                    c[1]+=1
+async def read_files():
+    counts=[0,0]
+    with open('File1.txt','r') as f:
+        data = f.read()
+        counts[0] = data.count('MARUTI')
+    with open('File2.txt','r') as f:
+        data = f.read()
+        counts[1] = data.count('MARUTI')
+    await write_count(counts)
 
-async def write_to_file(file_name,c):
+async def write_to_file(file_name):
     generated_string = random.choice([await generate_random(), 'MARUTI'])
     with open(file_name,'a') as f:
         f.write(generated_string+'\n')
 
 async def main():
+    print('Code execution started ...')
+    print('Press Ctrl + Z to stop')
     while True:
-        c = [0, 0]
-        t1 = asyncio.create_task(write_to_file('File1.txt',c))
-        t2 = asyncio.create_task(write_to_file('File2.txt',c))
-        t3 = asyncio.create_task(read_files('File1.txt',c))
-        t4 = asyncio.create_task(read_files('File2.txt',c))
-        t5 = asyncio.create_task(write_count(c))
+        t1 = asyncio.create_task(write_to_file('File1.txt'))
+        t2 = asyncio.create_task(write_to_file('File2.txt'))
+        t3 = asyncio.create_task(read_files())
 
-        await asyncio.gather(t1, t2, t3, t4, t5)
+        await asyncio.gather(t1, t2, t3)
 
 asyncio.run(main())
